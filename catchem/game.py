165177ac37478c9af1d1f65b20889ae4751932c2
Natanael-Mota15 <natanael.mota@csedge.org>
2022-10-22 16:05:54 -0400

    day 1

--- catchem/game.py
+++ catchem/game.py
@@ -11,18 +11,19 @@ class Game:
         self.speed = 0.5
         self.berry = []
         self.game_over = False
-        self.player = Player(56,63)
+        self.player = Player(56,63,sense)
 
 
     def start(self):
-        sense.shoe_message("Catchem",text_colour=colors.o,scroll_speed=0.05)
-
+        sense.show_message("Catchem",text_colour=(0,0,255),scroll_speed=0.05)
+        
 
     def play(self):
         self.start()
+        self.player.display()
         while not self.game_over:
             for event in sense.stick.get_events():
                 if event.action == "pressed" and event.direction == "left":
-                    print("left")
+                    self.player.move_left()
                 elif event.action == "pressed" and event.direction == "right":
-                        print("right")
+                    self.player.move_right()
--- catchem/player.py
+++ catchem/player.py
@@ -1,32 +1,31 @@
 import random
 from turtle import position
 
+from sense_hat import SenseHat
+Sense=SenseHat()
+
 
 
 
 class Player:
-    def __init__(self, left, right):
-        
-    
-        self.left = left
-        self.right = right
-        self.position = random.randint(left, right)
-        
+    def __init__(self, limit_l, limit_r, sense):
+        self.limit_r = limit_r
+        self.limit_l = limit_l
+        self.position = random.randint(limit_l, limit_r)
+        self.Sense=sense
 
     def move_right(self):
-        if position < self.right:
-            self.position=self.position+1
+        if position < self.limit_r:
+            self.position=self.position-1
+            display(1)
 
     def move_left(self):
-        if position > self.left:
-            self.position=self.position-1
+        if position > self.limit_l:
+            self.position=self.position+1
+            display(-1)
 
     def display(self):
         y=7
         x=self.position%8
-
-
-    def __init__(self,limit_l, limit_r):
-        self.limit_r = limit_r
-        self.limit_l = limit_l
-        self.position = random.randint(limit_l, limit_r)
+        self.Sense.set_pixel(x-move,y,(5,5,5))
+        self.Sense.set_pixel(x,y-1,(255,255,255))
